c8f035bd326f225bba1e6c1e77176323da8591a9
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-07-30 14:51:23 -0300

    Create Overlay on form submit sucess

--- src/pages/CreatePoint/index.js
+++ src/pages/CreatePoint/index.js
@@ -1,23 +1,32 @@
 import React from 'react'
-import { FiArrowLeft } from 'react-icons/fi'
+import { FiArrowLeft, FiCheckCircle } from 'react-icons/fi'
 
 import Container from '../../components/Container'
 import Header from '../../components/Header'
 import Link from '../../components/Link'
 import Form from '../../components/Form'
+import Overlay from '../../components/Overlay'
 
 import './style.css'
 
 function CreatePoint() {
   return (
-    <Container>
-      <Header>
-        <Link to="/" icon={FiArrowLeft} content="Voltar para home" />
-      </Header>
-      <div className="container-form">
-        <Form />
-      </div>
-    </Container>
+    <>
+      <Container>
+        <Header>
+          <Link to="/" icon={FiArrowLeft} content="Voltar para home" />
+        </Header>
+        <div className="container-form">
+          <Form />
+        </div>
+      </Container>
+      <Overlay>
+        <div className="box">
+          <FiCheckCircle className="box__icon" />
+          <span className="box__text">Cadastro concluído!</span>
+        </div>
+      </Overlay>
+    </>
   )
 }
 
