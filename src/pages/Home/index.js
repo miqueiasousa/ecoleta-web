b36d455b594067e0f6cb7309ac2b3c39dfc8dd0e
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-08-03 15:22:49 -0300

    Create form search

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FiLogIn, FiSearch } from 'react-icons/fi'
+import { getUfs, getCities } from '../../services/ibge'
 import Container from '../../components/Container'
 import Header from '../../components/Header'
 import Link from '../../components/Link'
+import Overlay from '../../components/Overlay'
+
 import './style.css'
 
 function Home() {
+  const [show, setShow] = useState(false)
+  const [ufs, setUfs] = useState([])
+  const [selectedUf, setSelectedUf] = useState('')
+  const [cities, setCities] = useState([])
+  // eslint-disable-next-line no-unused-vars
+  const [selectedCity, setSelectedCity] = useState('')
+
+  const handleSelectedUf = ({ target }) => {
+    setSelectedUf(target.value)
+  }
+
+  const handleSelectedCity = ({ target }) => {
+    setSelectedCity(target.value)
+  }
+
+  useEffect(() => {
+    getUfs().then(ufs => {
+      const serializedUfs = ufs.map(uf => uf.sigla)
+
+      setUfs(serializedUfs)
+    })
+  }, [])
+
+  useEffect(() => {
+    getCities(selectedUf).then(cities => {
+      const serializedCities = cities.map(city => city.nome)
+
+      setCities(serializedCities)
+    })
+  }, [selectedUf])
+
   return (
     <div className="wrapper">
       <Container>
@@ -26,7 +60,7 @@ function Home() {
               eficiente.
             </p>
           </div>
-          <div className="button-search">
+          <div className="button-search" onClick={() => setShow(true)}>
             <span className="button-search__container-icon">
               <FiSearch className="button-search__icon" />
             </span>
@@ -36,6 +70,42 @@ function Home() {
           </div>
         </main>
       </Container>
+      <Overlay show={show}>
+        <form className="form-search">
+          <h1 className="form-search__head">Pontos de coleta</h1>
+          <div className="fieldset-search">
+            <select
+              className="fieldset-search__input"
+              name="uf"
+              id="uf"
+              onChange={handleSelectedUf}
+            >
+              <option value="0">Selecione um estado</option>
+              {ufs.map(uf => (
+                <option key={uf} value={uf}>
+                  {uf}
+                </option>
+              ))}
+            </select>
+            <select
+              className="fieldset-search__input"
+              name="city"
+              id="city"
+              onChange={handleSelectedCity}
+            >
+              <option value="0">Selecione uma cidade</option>
+              {cities.map(city => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button className="form-search__btn" type="submit">
+            Buscar
+          </button>
+        </form>
+      </Overlay>
     </div>
   )
 }
