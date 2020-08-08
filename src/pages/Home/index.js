0ede85dfdcd3e3f0ef53999cb62d071a3d058cbb
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-08-07 22:18:16 -0300

    Implements OverlaySearch Component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,57 +1,14 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState } from 'react'
 import { FiLogIn, FiSearch } from 'react-icons/fi'
-import { useHistory } from 'react-router-dom'
-import { getPoints } from '../../services/api'
-import { getUfs, getCities } from '../../services/ibge'
 import Container from '../../components/Container'
 import Header from '../../components/Header'
 import Link from '../../components/Link'
-import Overlay from '../../components/Overlay'
-import { PointsContext } from '../../context/PointsContext'
+import OverlaySearch from '../../components/OverlaySearch'
 
 import './style.css'
 
 function Home() {
   const [show, setShow] = useState(false)
-  const [ufs, setUfs] = useState([])
-  const [selectedUf, setSelectedUf] = useState('')
-  const [cities, setCities] = useState([])
-  const [selectedCity, setSelectedCity] = useState('')
-  const [, setPoints] = useContext(PointsContext)
-  const history = useHistory()
-
-  const handleSelectedUf = ({ target }) => {
-    setSelectedUf(target.value)
-  }
-
-  const handleSelectedCity = ({ target }) => {
-    setSelectedCity(target.value)
-  }
-
-  const handleSubmit = event => {
-    event.preventDefault()
-
-    getPoints(selectedUf, selectedCity).then(points => {
-      setPoints(points)
-      history.push('/points')
-    })
-  }
-
-  useEffect(() => {
-    getUfs().then(ufs => {
-      const serializedUfs = ufs.map(uf => uf.sigla)
-
-      setUfs(serializedUfs)
-    })
-  }, [])
-
-  useEffect(() => {
-    getCities(selectedUf).then(cities => {
-      const serializedCities = cities.map(city => city.nome)
-
-      setCities(serializedCities)
-    })
-  }, [selectedUf])
 
   return (
     <div className="wrapper">
@@ -83,42 +40,7 @@ function Home() {
           </div>
         </main>
       </Container>
-      <Overlay show={show}>
-        <form className="form-search" onSubmit={handleSubmit}>
-          <h1 className="form-search__head">Pontos de coleta</h1>
-          <div className="fieldset-search">
-            <select
-              className="fieldset-search__input"
-              name="uf"
-              id="uf"
-              onChange={handleSelectedUf}
-            >
-              <option value="0">Selecione um estado</option>
-              {ufs.map(uf => (
-                <option key={uf} value={uf}>
-                  {uf}
-                </option>
-              ))}
-            </select>
-            <select
-              className="fieldset-search__input"
-              name="city"
-              id="city"
-              onChange={handleSelectedCity}
-            >
-              <option value="0">Selecione uma cidade</option>
-              {cities.map(city => (
-                <option key={city} value={city}>
-                  {city}
-                </option>
-              ))}
-            </select>
-          </div>
-          <button className="form-search__btn" type="submit">
-            Buscar
-          </button>
-        </form>
-      </Overlay>
+      <OverlaySearch showOverlay={show} />
     </div>
   )
 }
