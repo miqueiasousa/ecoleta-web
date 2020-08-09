49df7525c1bc5e0d7560049d7772f79995fe470e
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-08-08 22:00:42 -0300

    Push Down Exports

--- src/services/api.js
+++ src/services/api.js
@@ -4,20 +4,22 @@ const api = axios.create({
   baseURL: 'http://localhost:3030'
 })
 
-export const getItems = async () => {
+const getItems = async () => {
   const res = await api.get('items')
 
   return res.data
 }
 
-export const postPoint = async data => {
+const postPoint = async data => {
   const res = await api.post('points', data)
 
   return res.headers.location
 }
 
-export const getPoints = async (uf, city) => {
+const getPoints = async (uf, city) => {
   const res = await api.get(`points?uf=${uf}&city=${city}`)
 
   return res.data
 }
+
+export { getItems, postPoint, getPoints }
--- src/services/ibge.js
+++ src/services/ibge.js
@@ -4,14 +4,16 @@ const ibge = axios.create({
   baseURL: 'https://servicodados.ibge.gov.br/api/v1/localidades'
 })
 
-export const getUfs = async () => {
+const getUfs = async () => {
   const res = await ibge.get('estados')
 
   return res.data
 }
 
-export const getCities = async uf => {
+const getCities = async uf => {
   const res = await ibge.get(`estados/${uf}/municipios`)
 
   return res.data
 }
+
+export { getUfs, getCities }
